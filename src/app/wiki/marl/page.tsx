464f7be59a96afedf227833eb66d8546e2795eea
dauntss <kat.krahulec@gmail.com>
2025-01-27 13:23:42 -0700

    added last images and marl wiki page

---
+++ src/app/wiki/marl/page.tsx
@@ -0,0 +1,15 @@
+import Wiki from '../../components/wiki';
+
+export default function Page() {
+  return (
+    <div>
+      <Wiki
+        kingdom="Grand Duchy of Marl"
+        img="/images/wikiimages/marl.jpg"
+        capital="Warlisham"
+        climate="A mix of temperate forests and marshlands, with humid summers and cool, damp winters."
+        demographics="Mostly humans, with a notable population of elves living in the forested regions. Marl is known for its skilled hunters, trappers, and woodworkers."
+      />
+    </div>
+  );
+}
